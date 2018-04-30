c8e5d80051c2d4fec2f18fc4e932cfa1e1fd557b
Seo Won Kyo <skaty4870@likelion.org>
2018-04-30 15:15:21 +0000

    .

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,4 +1,11 @@
 class MailController < ApplicationController
     def index
     end
+    
+    def sending
+        @r_email = params[:r_email]
+        @s_email = params[:s_email]
+        @title = params[:title]
+        @content = params[:content]
+    end
 end
